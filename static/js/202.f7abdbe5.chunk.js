f6858300938bf63490f256601bb3e7914bafe963
Dranyk <96312895+Dranyk@users.noreply.github.com>
2023-05-20 18:55:14 +0000

    Deploying to gh-pages from @ Dranyk/YourPet@d40865f129aa67ae5a9babf9817a5cae3ac4fc88 🚀

---
+++ static/js/202.f7abdbe5.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkyour_pet=self.webpackChunkyour_pet||[]).push([[202],{9202:function(e,u,t){t.r(u);var n=t(3329);u.default=function(){return(0,n.jsx)("p",{children:"Notices Page"})}}}]);
+//# sourceMappingURL=202.f7abdbe5.chunk.js.map
